1c95f7d799a8e581bc35aac5fc656c61b1a54c54
masha <Masha_Dubach@sluz.ch>
2024-11-06 11:27:22 +0100

    zusammengesetzt

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
- 
+ function go(){
+    let text =  String(document.getElementById("eingabe").value); 
+    
+    if(text.includes("müde")){
+        document.getElementById("linkesAuge").innerHTML = "o"
+        document.getElementById("rechtesAuge").innerHTML = "o"
+    }
+
+    if(text.include("hässig")){
+        document.getElementById("gesicht").style.backgroundColor = "red";
+    }
+
+    if(text.include("zwinkern")){
+        document.getElementById("linkesAuge").innerHTML = "-"
+    }
+
+    if(text.include("schmollen")){
+        document.getElementById("mund").innerHTML = "-"
+        document.getElementById("mund").style.width = "25%"; 
+        document.getElementById("mund").style.marginLeft = "35%"; 
+    }
+ }
 
